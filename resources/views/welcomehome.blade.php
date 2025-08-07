a1268fc08832b340c2d509c1b0a0642058f77c0c
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2025-08-07 15:39:19 +0300

    first commit

---
+++ resources/views/welcomehome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>something</title>
+</head>
+<body>
+    <h2>I swear to god I never fall in love , then you show up and I can't have enough of it</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur cupiditate suscipit fuga. 
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae iure id veritatis et sequi placeat autem voluptatum maxime, cupiditate earum?</p>
+</body>
+</html>
