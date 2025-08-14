cd4965397397647c172e70adc3ddb9ae96f6ac9e
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2025-08-14 12:17:21 +0300

    applaying components and using tailwind css

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <div>
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, reiciendis.</p>
+    </div>
+    <div>
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, reiciendis.</p>
+    </div>
+    <div>
+        <h2>Lorem, ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, reiciendis.</p>
+    </div>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    <ul>
+        <li>123456789</li>
+        <li>hamzaalchamak442979@gmail.com</li>
+        <li>alnafl distract</li>
+    </ul>
+</x-layout>
