8c766a2170b3e3a256109a1a6c038ebf2428b706
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2025-11-20 18:11:53 +0300

    creating paginations and seeders

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,18 @@
     <x-slot:heading>
         Jobs Page
     </x-slot:heading>
-        <ul>
+        <div class="space-y-4">
             @foreach($jobs as $job)
-                <li>
-                    <a href="/jobs/{{ $job['id'] }}">
-                        <strong>{{ $job['title']; }}</strong> pays ${{ $job['salary']; }}
-                    </a>
-                </li>
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-400 rounded-lg">
+                    <div class="font-bold text-blue-500">{{ $job->employer->name }}</div>
+                    <div>
+                        <strong>{{ $job['title']; }}</strong> pays ${{ $job['salary']; }} per year
+                    </div>
+                </a>
             @endforeach
-        </ul>
+            
+            <div>
+                {{ $jobs->links() }}
+            </div>
+        </div>
 </x-layout>
