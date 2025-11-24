6d16b964846b70c5fde6506b048c1d33c8ec1725
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2025-11-24 10:10:19 +0300

    validation on job creation form

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -42,8 +42,9 @@
     </nav>
   
     <header class="relative bg-white shadow-sm">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between ">
         <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+        <x-button href="/jobs/create" class="mt-4 sm:mt-0">Create Job</x-button>
       </div>
     </header>
     <main>
